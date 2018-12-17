1b5b169e910b132e41a630ccad55ca516996f1a4
fullstackwebdev <fullstackwebdev@gmail.com>
2018-12-17 10:39:32 -0800

    Cleanup and switch to p-timeout

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 'use strict';
-/* eslint-disable id-length, prefer-promise-reject-errors, arrow-body-style */
-
-const Timeout = require('await-timeout');
+/* eslint-disable id-length, no-else-return */
 const { r } = require('rethinkdb-ts');
+const pTimeout = require('p-timeout');
 const pkg = require('./package.json');
 
 const register = async (server, options) => {
@@ -10,7 +9,7 @@ const register = async (server, options) => {
     options.pingInterval = options.pingInterval || 1;
 
     let conn;
-    async function reconnect() {
+    const reconnect = async () => {
         if (options.useConnPool) {
             conn = await r.connectPool(options);
         }
@@ -18,12 +17,12 @@ const register = async (server, options) => {
             try {
                 conn = await r.connect(options);
             }
-            catch (error) {
-                console.error(error.cause);
-                throw error;
+            catch (err) {
+                console.error(err.cause);
+                throw err;
             }
         }
-    }
+    };
     // Hapi Inject doesn't wait for plugins to load nor sends 'start' event, so we try to connect
     await reconnect();
 
@@ -36,92 +35,38 @@ const register = async (server, options) => {
     server.decorate('server', 'db', async (query) => {
         let result;
 
-        if (options.useConnPool) {
-            if (!conn.isHealthy) {
-                await reconnect();
-            }
-            try {
-            // A .wait is broken in rethinkdb
-            // Run query, timeout
-                const timer = new Timeout();
-                try {
-                    result = await Promise.race([
-                        query.run(), // Connection pool
-                        timer.set(queryTimeoutMs)
-                            .then(() => Promise.reject('timeout'))
-                    ]);
-                }
-                finally {
-                    timer.clear();
-                }
-            }
-            catch (err) {
-                if (err === 'timeout') {
-                    throw new Error('Timeout exceeded for query');
-                }
-                else {
-                    throw err;
-                }
-            }
+        if (!conn.isHealthy) {
+            await reconnect();
         }
-        else {
-            try {
-                const timer = new Timeout();
-                try {
-                    result = await Promise.race([
-                        r.expr(query).run(conn), // Non-connection pool
-                        timer.set(queryTimeoutMs)
-                            .then(() => Promise.reject('timeout'))
-                    ]);
-                }
-                finally {
-                    timer.clear();
-                }
+        try {
+            result = pTimeout(
+                (() => {
+                    if (options.useConnPool) {
+                        return query.run();
+                    }
+                    else {
+                        return r.expr(query).run(conn);
+                    }
+                })(), queryTimeoutMs);
+        }
+        catch (err) {
+            if (err.name === 'TimeoutError') {
+                throw new Error('Timeout exceeded for query');
             }
-            catch (err) {
-                if (err === 'timeout') {
-                    throw new Error('Timeout exceeded for query');
-                }
-                else {
-                    throw err;
-                }
+            else {
+                throw err;
             }
         }
 
         return result;
     });
 
-    server.decorate('server', 'dbCursor', async (query) => {
-        let cursor;
-        try {
-            const timer = new Timeout();
-            try {
-                cursor = await Promise.race([
-                    query.getCursor(),
-                    timer.set(queryTimeoutMs)
-                        .then(() => Promise.reject('timeout'))
-                ]);
-            }
-            finally {
-                timer.clear();
-            }
-        }
-        catch (err) {
-            if (err === 'timeout') {
-                throw new Error('Timeout exceeded for query');
-            }
-            else {
-                console.error(err);
-                throw err;
-            }
-        }
-        return {
-            cursor
-        };
-    });
     server.events.on('stop', () => {
         if (options.useConnPool) {
             r.getPoolMaster().drain();
+        }
+        else {
+            conn.close();
         }
     });
 };
